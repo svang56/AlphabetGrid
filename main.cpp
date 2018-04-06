86b79bfc4c90714bc57b4d8bb44d46376d3b19be
svang56 <svang56@mail.csuchico.edu>
2018-04-06 15:38:35 -0700

    Hua

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>//Author: Sunny Vang
 #include <vector>
 
 using namespace std;
@@ -6,6 +6,9 @@ using namespace std;
 int main()
 {
   int letters;
+
+  vector <vector <char> > alphabet;
+  
 
   do
   {
@@ -18,5 +21,48 @@ int main()
     }
   }while( letters < 1 || letters > 26 );
 
+  int rows;
+  if( 26 % letters == 0)
+  {
+    rows = 26 / letters;
+  }
+  else
+  {
+    rows = 26 / letters + 1;
+  }
+
+ //resize rid to have letter # of rows, and _columns_ # of columns
+  alphabet.resize(rows);
+  
+  for (int row =0; row < alphabet.size(); row++)
+  {
+    alphabet[row].resize(letters);
+  }
+char currentLetter = 'A';
+
+ //traverse the matric and assign it incrementings letters from A to Z
+ for(int row = 0; row < alphabet.size(); row++)
+ {
+    for(int col = 0; col < alphabet[row].size(); col++)
+    {
+        alphabet[row][col] = currentLetter;
+        currentLetter++;
+        if(currentLetter > 'Z')
+        {
+            currentLetter = 'Z';//makes letter go to next lette in ASCII table
+        }
+    }
+  }
+ for(int row = 0; row < alphabet.size(); row++)//dispplay matrix
+ {
+    for(int col = 0; col < alphabet[row].size(); col++)
+    {
+    cout<<alphabet[row][col];
+    }
+    cout<<endl;
+} 
+ 
   return 0;
 }
+
+
